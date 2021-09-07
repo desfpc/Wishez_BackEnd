6272d1a29e25a5ef7e2d92898d3176fbe93cd79e
Sergey Peshalov <desfpc@gmail.com>
2021-09-07 09:25:42 +0300

    Delete docs directory

--- docs/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-git fetch --all
-git checkout --force "origin/master"
-systemctl stop wishez-backend.service
-go build /root/wishez-backend/main.go
-go install /root/wishez-backend/main.go
-systemctl start wishez-backend.service
